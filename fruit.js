d87547d072850e5afacae61e0d70d51e4e5f3664
unknown <s.brohi@uq.net.au>
2020-02-04 03:34:16 +1000

    Completed game

--- fruit.js
+++ fruit.js
@@ -2,14 +2,13 @@ function Fruit() {
     this.x = 0;
     this.y = 0;
 
-    this.updateCoords = function() {
+    this.updateCoords = function () {
         this.x = Math.floor((Math.random() * rows - 1) * scale);
         this.y = Math.floor((Math.random() * columns - 1) * scale);
-        console.log(this.x, this.y)
     }
 
-    this.draw = function() {
-        ctx.fillStyle = "red";
+    this.draw = function () {
+        ctx.fillStyle = "green";
         ctx.fillRect(this.x, this.y, scale, scale);
     }
 
--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 const CANVAS = document.querySelector("canvas");
+const SCORE = document.querySelector(".score");
 
 let ctx = CANVAS.getContext("2d");
 let scale = 20;
-let rows = CANVAS.width/scale;
-let columns = CANVAS.height/scale;
-let snake = new Snake();
-let fruit  = new Fruit();
-fruit.updateCoords();
-console.log(fruit.x, fruit.y)
+let rows = CANVAS.width / scale;
+let columns = CANVAS.height / scale;
 let direction = "Right";
 
+(function setup() {
+    let snake = new Snake();
+    let fruit = new Fruit();
+    fruit.updateCoords();
 
+    setInterval(
+        () => {
+            ctx.clearRect(0, 0, CANVAS.height, CANVAS.width);
+            fruit.draw();
 
-setInterval(
-    () => {
-        ctx.clearRect(0, 0, CANVAS.height, CANVAS.width);
-        fruit.draw();
+            snake.draw();
+            snake.move();
+            snake.updateDirection();
+            snake.checkCollision();
 
-        snake.draw();
-        snake.move();
-        snake.updateDirection();
+            SCORE.innerText = snake.totalScore;
 
-    }, 100
-)
+            if ((snake.x < fruit.x + scale) &&
+                (snake.x + scale > fruit.x) &&
+                (snake.y < fruit.y + scale) &&
+                (snake.y + scale > fruit.y)) {
+                    fruit.updateCoords();
+                    snake.totalScore += 1;
+            }
+        }, 100
+    )
+})();
 
 addEventListener("keydown", (e) => {
-    direction = e.key.replace("Arrow", "");
+    console.log(e.key)
+    if (e.key == "ArrowUp" && direction != "Down") {
+        direction = e.key.replace("Arrow", "");
+    } else if (e.key == "ArrowDown" && direction != "Up") {
+        direction = e.key.replace("Arrow", "");
+    } else if (e.key == "ArrowLeft" && direction != "Right") {
+        direction = e.key.replace("Arrow", "");
+    } else if (e.key == "ArrowRight" && direction != "Left") {
+        direction = e.key.replace("Arrow", "");
+    }
 }, false)
