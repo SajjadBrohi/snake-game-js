889daae28334cf1a148ac05ecc382be21242c2d3
SajjadBrohi <s.brohi@uq.net.au>
2020-08-13 02:17:20 +1000

    Add various TypeScript examples

--- app.js
+++ app.js
@@ -2,7 +2,27 @@
 let y;
 y = 3;
 let z = 3;
-// Done in class style; here the type can be either bold or italic or 23, so that shows we are not restricted to one type when assigning
-let font;
-font = 'something else'; // Gives error
-font = 'bold'; // Doesn't give error
+const person = {
+    firstName: '3',
+    lastName: 'no',
+    age: 3,
+    refer: 'no',
+};
+// Each parameter needs to have the type that you want as the arguments, and after the parameters you mention the type that you want returned
+// You can use the type 'void' as return type to not return anything. typically used on eventlisteners or sideeffects
+function names(first, second) {
+    return first + second;
+}
+names('sajjad ', 'brohi');
+// Note that 'Person' is an interface with the types of the properties declared
+const arr = [];
+arr.push(person);
+arr.push(3); //Error
+const myList = []; //If we don't put question mark when declaring the type above we will have to initialise the array here
+myList.push(3);
+myList.push(3);
+myList.push(3);
+class Rush {
+    constructor(name) { }
+}
+let x = new Rush(arr); //Implicitly declaring the type to be used by using an argument of an array of 'person' objects in the constructor
--- app.ts
+++ app.ts
@@ -6,10 +6,45 @@ y = 3;
 
 let z = 3;
 
-type Style = 'bold' | 'italic' | 23;
-// Done in class style; here the type can be either bold or italic or 23, so that shows we are not restricted to one type when assigning
+interface Person {
+	firstName: String;
+	lastName: String;
+	age: Number;
+	[key: string]: any;
+	// aDDING LAST LINE means that every object implementing this interface will NEED to have the first three properties, and then it can add any more if it wants
+}
 
-let font: Style;
+const person: Person = {
+	firstName: '3',
+	lastName: 'no',
+	age: 3,
+	refer: 'no', //DOESN'T WORK UNLESS WE USE LAST LLINE OF INTERFACE
+};
 
-font = 'something else'; // Gives error
-font = 'bold'; // Doesn't give error
+// Each parameter needs to have the type that you want as the arguments, and after the parameters you mention the type that you want returned
+// You can use the type 'void' as return type to not return anything. typically used on eventlisteners or sideeffects
+function names(first: string, second: string): string {
+	return first + second;
+}
+
+names('sajjad ', 'brohi');
+
+// Note that 'Person' is an interface with the types of the properties declared
+const arr: Person[] = [];
+
+arr.push(person);
+arr.push(3); //Error
+
+type MyList = [number?, string?, boolean?];
+
+const myList: MyList = []; //If we don't put question mark when declaring the type above we will have to initialise the array here
+
+myList.push(3);
+myList.push(3);
+myList.push(3);
+
+class Rush<T> {
+	constructor(name: T) {}
+}
+
+let x = new Rush(arr); //Implicitly declaring the type to be used by using an argument of an array of 'person' objects in the constructor
